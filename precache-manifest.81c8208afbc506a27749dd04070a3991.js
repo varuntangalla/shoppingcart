ee1d953770346c286e1f6fe41588a665f71a12b6
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-10-14 20:07:46 -0700

    Updates

---
+++ precache-manifest.81c8208afbc506a27749dd04070a3991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1bd0ddf5d1f71106fcc80d0e14463f0",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "f49f66f5bc5a5f9317d9",
+    "url": "/shoppingcart/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "9a08d56bb55dad7d7159",
+    "url": "/shoppingcart/static/css/main.3aefb40b.chunk.css"
+  },
+  {
+    "revision": "f49f66f5bc5a5f9317d9",
+    "url": "/shoppingcart/static/js/2.9e7d88ec.chunk.js"
+  },
+  {
+    "revision": "9a08d56bb55dad7d7159",
+    "url": "/shoppingcart/static/js/main.c6903163.chunk.js"
+  },
+  {
+    "revision": "ddcc1a4b2490516eaa45",
+    "url": "/shoppingcart/static/js/runtime-main.a0c9828b.js"
+  }
+]);
